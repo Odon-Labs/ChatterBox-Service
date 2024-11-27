c946b346b97b650407628d954775d10c0357b5fb
Shubham-handa <49878873+Shubham-handa@users.noreply.github.com>
2024-11-28 01:00:13 +0530

    WIP: Application running successfully

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+dotenv.config();
+
 //const connectDB = require('./main/config/db');
-const authRoutes = require('./main/routes/authRoutes');
+//const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
 
 // Middleware
 app.use(express.json());
-app.use('/api/auth', authRoutes);
+//app.use('/api/auth', authRoutes);
 
+// var usp = io.of('/user-namespace');
 // Socket.IO connection
 io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
@@ -29,7 +34,8 @@ io.on('connection', (socket) => {
 // Database connection
 // connectDB();
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9095;
+
 mongoose.connect('mongodb://localhost:27017/chatDatabase')
 .then(()=>{
     app.listen(PORT,()=>console.log(`Listening on port ${PORT}`))
